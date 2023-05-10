b9ebed2cad320c2d4d51bf0e76e417820f5a4b59
lbcamp1117 <125312035+lbcamp1117@users.noreply.github.com>
2023-05-09 19:47:04 -0500

    Update generateMarkdown.js

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,10 +1,35 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === "Apache 2.0 License") {
+    return "[![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)";
+  } else if (license === "MIT License") {
+    return "[![License](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)";
+  } else if (license === "Boost Software License 1.0") {
+    return "[![License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)](https://www.boost.org/LICENSE_1_0.txt)";
+  } else if (license === "BSD 3-Clause License") {
+    return "[![License](https://img.shields.io/badge/License-BSD_3--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)";
+  } else {
+    return "";
+  }
+}
+
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === "Apache 2.0 License") {
+    return "https://opensource.org/licenses/Apache-2.0";
+  } else if (license === "MIT License") {
+    return "https://opensource.org/licenses/MIT";
+  } else if (license === "Boost Software License 1.0") {
+    return "https://www.boost.org/LICENSE_1_0.txt";
+  } else if (license === "BSD 3-Clause License") {
+    return "https://opensource.org/licenses/BSD-3-Clause";
+  } else {
+    return "";
+  }
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
@@ -13,7 +38,16 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
+  ${renderLicenseBadge(data.license)}
+  ${renderLicenseLink(data.license)}<br />
+  ${data.description}<br />
+  ${data.installInfo}<br />
+  ${data.usageInfo}<br />
+  ${data.testingInfo}<br />
+  ${data.contributorsInfo}<br />
+  ${data.eMail}<br />
+  ${data.gitHub}<br />
+  
 `;
 }
 
